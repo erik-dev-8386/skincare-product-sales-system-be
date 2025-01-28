a9c6f5ec3df2c3f5d2c279cb65a4eb7cc061e7a5
erik-dev <141310212+Erik211204@users.noreply.github.com>
2025-01-28 21:06:28 +0700

    Delete src directory

--- src/main/java/application/havenskin/repository/BlogRepostiory.java
+++
@@ -1,9 +0,0 @@
-package application.havenskin.repository;
-
-import application.havenskin.BusinessObject.Models.Blogs;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepostiory extends JpaRepository<Blogs, String> {
-}
--- src/main/java/application/havenskin/repository/SkinTestsRepository.java
+++
@@ -1,7 +0,0 @@
-package application.havenskin.repository;
-
-import application.havenskin.BusinessObject.Models.SkinTests;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SkinTestsRepository extends JpaRepository<SkinTests, String> {
-}
