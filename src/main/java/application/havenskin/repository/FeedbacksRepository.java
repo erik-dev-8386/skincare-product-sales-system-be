c63dcc4483f50f4ffc76c53d3054280efedf59c6
erik-dev <141310212+erik-dev-8386@users.noreply.github.com>
2025-03-06 14:47:22 +0700

    Delete src directory

--- src/main/java/application/havenskin/repository/FeedbacksRepository.java
+++
@@ -1,9 +0,0 @@
-package application.havenskin.repository;
-
-import application.havenskin.BusinessObject.Models.Feedbacks;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FeedbacksRepository extends JpaRepository<Feedbacks,String> {
-}
--- src/main/java/application/havenskin/repository/OrderDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package application.havenskin.repository;
-
-import application.havenskin.BusinessObject.Models.OrderDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderDetailsRepository extends JpaRepository<OrderDetails, String> {
-}
