157545ea9b11c4b1b28289d9eef2bbfb45729153
HieuColdHead <155791877+HieuColdHead@users.noreply.github.com>
2025-04-01 13:15:50 +0700

    Delete src directory

--- src/main/java/application/havenskin/repository/ProductImagesRepository.java
+++
@@ -1,7 +0,0 @@
-package application.havenskin.repository;
-
-import application.havenskin.BusinessObject.Models.ProductImages;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductImagesRepository extends JpaRepository<ProductImages, String> {
-}
